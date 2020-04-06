9ae4b92e24382f07a842e9ad9c6e7d7714dbb46e
frozenparadox99 <frostbitecryengine300@gmail.com>
2020-04-06 16:42:17 +0530

    added auth middleware to protect routes

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,19 @@
 const express = require("express");
-
 const router = express.Router();
+
+const auth = require("../../middleware/auth");
 
 // -> Route    -> GET api/auth
 // -> Desc     -> Test route
 // -> Access   -> Public
-router.get("/", (req, res) => res.send("Auth Route"));
+router.get("/", auth, async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    res.json(user);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server Error");
+  }
+});
 
 module.exports = router;
