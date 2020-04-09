9bd122b7cc068857bba8707fbe77f455ad6e0600
frozenparadox99 <frostbitecryengine300@gmail.com>
2020-04-09 18:59:54 +0530

    added profile form component

--- client/src/App.js
+++ client/src/App.js
@@ -8,6 +8,7 @@ import Login from "./components/auth//Login";
 import Register from "./components/auth/Register";
 import Alert from "./components/layout/Alert";
 import Dashboard from "./components/dashboard/Dashboard";
+import ProfileForm from "./components/profile-forms/ProfileForm";
 import PrivateRoute from "./components/routing/PrivateRoute";
 
 import { Provider } from "react-redux";
@@ -39,6 +40,11 @@ function App() {
               <Route exact path="/register" component={Register} />
               <Route exact path="/login" component={Login} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
+              <PrivateRoute
+                exact
+                path="/create-profile"
+                component={ProfileForm}
+              />
             </Switch>
           </section>
         </Fragment>
